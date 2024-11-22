a40ea7cfebd620b78558281fd680c9a731982870
Tanushree227 <tanushreedas813@gmail.com>
2024-11-22 12:16:02 +0530

    first commit

---
+++ classObject.java
@@ -0,0 +1,24 @@
+class Car
+{
+    String color;
+    String model;
+    
+    Car(String color, String model)
+    {
+        this.color = color;
+        this.model = model;
+    }
+
+    void startEngine()
+    {
+        System.out.println("Engine Started... Vroom!");
+    } 
+}
+
+class classObject
+{
+    public static void main(String[] args) {
+        Car myCar = new Car("Black", "McLaren");
+        myCar.startEngine();
+    }
+}
